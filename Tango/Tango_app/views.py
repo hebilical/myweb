97a6cf3db2991a03987e73e8facf6d2102d5d826
hebilical <929040606@qq.com>
2017-10-17 21:12:46 +0800

    add templates

--- Tango/Tango_app/views.py
+++ Tango/Tango_app/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hollow world!')
+    mydict={'author':'hebilical'}
+    return render(request,'Tango_app/index.html',mydict)
 
 def about(request):
     return HttpResponse ('this is the about page !')
